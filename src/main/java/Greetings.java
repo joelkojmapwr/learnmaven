fdfb78ff6a80021d5449a2cca4f0f5041db14f3e
279745@student.pwr.edu.pl <joel.kojma.pwrrt@gmail.com>
2024-10-15 16:03:33 +0200

    update

--- src/main/java/Greetings.java
+++ src/main/java/Greetings.java
@@ -1,9 +1,18 @@
 package pt.trainings.maven.scratch;
 
+/**
+ * This class provides a method to generate greeting messages.
+ */
 public class Greetings {
     private static final String GREETING = "Hello ";
 
+    /**
+     * Provides a greeting message.
+
+     * @param string the name of the person to greet
+     * @return the greeting message
+     */
     public String sayHello(String string) {
-        return GREETING + (string != null ? string : "Jonh Doe");
+        return GREETING + (string != null ? string : "John Doe");
     }
 }
--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,7 +1,8 @@
 package pt.trainings.maven.scratch;
 
-public class HelloWorld {
+public class HelloWorld { 
     public static void main(String[] args) {
+        Integer i =5;
         Greetings greetings = new Greetings();
         System.out.println(greetings.sayHello(args.length > 0 ? args[0] : null));
     }
